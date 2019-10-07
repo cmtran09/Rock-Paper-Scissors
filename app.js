a924e99a9a620d9f11c8eb936d73f0b972cd3d04
cmtran09 <cmtran@hotmail.co.uk>
2019-10-07 14:53:41 +0100

    Player and computer hands added and fade in intro added.

--- app.js
+++ app.js
@@ -1,49 +1,101 @@
 const game = () => {
   const options = document.querySelectorAll(".options button");
+  // Possible computer options
   const comupterOptions = ["Rock", "Paper", "Scissors"];
+  let pScore = 0;
+  let cScore = 0;
 
+  // function to start bring in the game
+  const startGame = () => {
+    const startButton = document.querySelector(".start-game");
+    const introScreen = document.querySelector(".intro");
+    const scoreBoard = document.querySelector(".score");
+    const arena = document.querySelector(".arena");
+    const pCtrls = document.querySelector(".options");
+    const pMess = document.querySelector(".message1");
+
+    startButton.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      arena.classList.add("fadeIn");
+      scoreBoard.classList.add("fadeIn");
+      pMess.classList.add("fadeIn");
+      pCtrls.classList.add("fadeIn");
+    });
+  };
+
+  startGame();
+  // Go through each of the player option buttons and add an eventlister of click that ...
+  // runs a function on click
   options.forEach(options => {
     options.addEventListener("click", function() {
-      console.log(this);
+      const playerHand = document.querySelector(".player-hand");
+      const computerHand = document.querySelector(".computer-hand");
       // Create a random number between 0-1 mutliply by 3 (number of options in array)...
       // ... then round to nearest interger.
       const compIndex = Math.floor(Math.random() * 3);
+      // random interger between 0-2 used to select a string from the computerOptions array ...
       const computerChoice = comupterOptions[compIndex];
       console.log("comp" + computerChoice);
       console.log(this.textContent);
+      //update the hand images
+      playerHand.src = `./images/${this.textContent}.png`;
+      computerHand.src = `./images/${computerChoice}.png`;
+      // Run checker function passing in this.textContent (the contents of the button element) ...
+      // and the computer's random choice of rock paper or scissors from array
       checker(this.textContent, computerChoice);
+      console.log(pScore);
+      console.log(cScore);
+      // score updated at the end of each game
+      updateScore();
     });
   });
 
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    // changing the html element with the score
+    playerScore.innerHTML = pScore;
+    computerScore.innerHTML = cScore;
+  };
+
   const checker = (playerChoice, computerChoice) => {
     if (playerChoice === computerChoice) {
-      console.log("draw");
+      console.log("Draw");
       return;
     }
-    if (playerChoice === "rock") {
+    // When player clicks rock
+    if (playerChoice === "Rock") {
       if (computerChoice === "Scissors") {
-        console.log("you win");
+        console.log("You Win");
+        pScore++;
         return;
       } else if (computerChoice === "Rock") {
-        console.log("you lose");
+        console.log("You Lose");
+        cScore++;
         return;
       }
     }
+    // When player clicks scissors
     if (playerChoice === "Scissors") {
       if (computerChoice === "Paper") {
-        console.log("you win");
+        console.log("You Win");
+        pScore++;
         return;
       } else if (computerChoice === "Rock") {
-        console.log("you lose");
+        console.log("You Lose");
+        cScore++;
         return;
       }
     }
+    // When player clicks paper
     if (playerChoice === "Paper") {
       if (computerChoice === "Rock") {
-        console.log("you win");
+        console.log("You Win");
+        pScore++;
         return;
       } else if (computerChoice === "Scissors") {
-        console.log("you lose");
+        console.log("You Lose");
+        cScore++;
         return;
       }
     }
